7d97cf698e255bb4c7fad26e497849aba2a67a6e
mahesaref <mahesarefado30@gmail.com>
2022-09-15 20:11:28 +0700

    Menambahkan File

---
+++ let.js
@@ -0,0 +1,13 @@
+//Reassign
+let nama = "coco";
+let nama = "crunch";
+console.log(nama);
+
+//Hoisting doesn't apply 
+nama = "coco";
+let nama;
+console.log(nama);
+
+
+
+
---
+++ var.js
@@ -0,0 +1,20 @@
+// Menerapkan Scope
+var nama = "coco";
+var nama = "crunch";
+
+//creat function namaOrang
+function namaOrang(){
+    var nama = "crunch";
+    console.log(nama);
+}
+
+//display function namaOrang
+namaOrang();
+
+//display variable name
+console.log(nama);
+
+//Menerapkan Hoisting
+nama = "coco";
+console.log(nama);
+var nama
