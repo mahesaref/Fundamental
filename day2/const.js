f6d408487afb0a16d401f3488d3088334ffa7f44
mahesaref <mahesarefado30@gmail.com>
2022-09-21 21:13:48 +0700

    first commit

--- day2/const.js
+++ day2/const.js
@@ -1,12 +1,12 @@
 const PI = 3.141592653589793;
 const PI2 = 22/7;
-// PI = 3.14;     
-// PI = PI + 10;  
-// console.log(PI);
+
 const r=8;
 const r2=7;
-calculate = PI2*r2*r2; 
+calculate = PI*r*r;
+calculatelain = PI2*r2*r2; 
 console.log(calculate);
+console.log(calculatelain);
 
 
 
--- day2/undefined.js
+++ day2/undefined.js
@@ -1,5 +1,5 @@
 var x;
-var y = '1';
+var y = 1;
 
 console.log(x); // undefined
 console.log(y);
